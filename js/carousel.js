c177894a3b85f1d1aa934350bf6ffe194f3025ff
Bison Hubert <davidbisonhubert@gmail.com>
2017-07-04 11:11:51 -0700

    Add preliminary carousel

---
+++ js/carousel.js
@@ -0,0 +1,21 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+  var slides = document.getElementsByClassName("quoteCarouselBlock");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (var i = 0; i < slides.length; i++) {
+    // refactor to forEach
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
