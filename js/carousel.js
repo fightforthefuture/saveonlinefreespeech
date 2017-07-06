a38672d08b47d5067b761934c8e919174d97d10f
Bison Hubert <davidbisonhubert@gmail.com>
2017-07-05 17:39:57 -0700

    Improve carousel scope, event handling, mutation

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,45 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function nextSlide(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n)
-}
-
-function showSlides(n) {
+(function() {
+  document.querySelector(".quote-carousel .next").addEventListener("click", nextSlide);
+  document.querySelector(".quote-carousel .prev").addEventListener("click", previousSlide);
+  
   var slides = document.getElementsByClassName("quoteCarouselBlock");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  var slideIndex = 0;
+  showSlides();
+
+  function nextSlide() {
+    setSlide(slideIndex + 1);
   }
-  slides[slideIndex - 1].style.display = "block";
-}
+
+  function previousSlide() {
+    setSlide(slideIndex - 1);
+  }
+
+  function hideSlide(slide) {
+    slide.classList.remove("shown");
+    slide.classList.add("hidden");
+  }
+
+  function showSlide(slide) {
+    slide.classList.remove("hidden");
+    slide.classList.add("shown");
+  }
+
+  function setSlide(n) {
+    if (n < 0) {
+      slideIndex = slides.length - 1
+    } else if (n >= slides.length) {
+      slideIndex = 0
+    } else {
+      slideIndex = n
+    }
+    
+    showSlides();
+  }
+
+  function showSlides() {
+    for (var i = 0; i < slides.length; i++) {
+      hideSlide(slides[i]);
+    }
+    showSlide(slides[slideIndex]);
+  }
+})();
