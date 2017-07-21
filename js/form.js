d2679aa22965fe1ec6af918a6a5c6d7f16fdd618
Bison Hubert <davidbisonhubert@gmail.com>
2017-07-20 20:28:25 -0700

    Show modal on success form submission

--- js/form.js
+++ js/form.js
@@ -1,11 +1,7 @@
 (function() {
-  var modal = document.getElementsByClassName('success-modal')[0];
-  var button = document.getElementsByClassName('form-button')[0];
-  var span = document.getElementsByClassName('modal-close')[0]
-
-  button.onclick = function() {
-    modal.classList.remove("hidden");
-  }
+  var modal = document.getElementsByClassName("success-modal")[0];
+  var button = document.getElementsByClassName("form-button")[0];
+  var span = document.getElementsByClassName("modal-close")[0]
 
   span.onclick = function() {
     modal.classList.add("hidden");
@@ -17,13 +13,17 @@
     }
   }
 
-  var form = document.getElementsByClassName('petition-form')[0];
-  form.addEventListener('submit', function submitForm(e) {
+  var form = document.getElementsByClassName("petition-form")[0];
+  form.addEventListener("submit", function submitForm(e) {
     e.preventDefault();
     var formData = new FormData(form);
     var req = new XMLHttpRequest();
 
-    req.open(form.getAttribute('method'), form.getAttribute('action'));
+    req.addEventListener("load", function(event) {
+      modal.classList.remove("hidden");
+    });
+
+    req.open(form.getAttribute("method"), form.getAttribute("action"));
     req.send(formData);
   });
 })();
