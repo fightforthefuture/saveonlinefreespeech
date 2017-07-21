d8c1a85681fb302c7f6566fb1443dbca4c443e8b
Bison Hubert <davidbisonhubert@gmail.com>
2017-07-20 19:09:26 -0700

    Hide modal until form submitted

--- js/form.js
+++ js/form.js
@@ -1,4 +1,16 @@
 (function() {
+  var modal = document.getElementsByClassName('success-modal')[0];
+  var button = document.getElementsByClassName('form-button')[0];
+  var span = document.getElementsByClassName('modal-close')[0]
+
+  button.onclick = function() {
+    modal.classList.remove("hidden");
+  }
+
+  span.onclick = function() {
+    modal.classList.add("hidden");
+  }
+
   var form = document.getElementsByClassName('petition-form')[0];
   form.addEventListener('submit', function submitForm(e) {
     e.preventDefault();
